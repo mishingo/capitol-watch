7cf01fdbf2db71a521ae7a6c0aff8d23d9ce5c09
mishingo <mr@mishingo.com>
2015-04-17 18:04:42 -0400

    create bills to watch component

--- client/app/bills/bills.controller.js
+++ client/app/bills/bills.controller.js
@@ -10,14 +10,23 @@ angular.module('capitolwatchApp')
     $scope.message = 'Hello Bitch';
     $scope.id = $routeParams.id;
     var billid = $routeParams.id;
-    console.log($scope.id);
+    //console.log($scope.id);
 
     
-    $http.jsonp('https://www.govtrack.us/api/v2/bill/' + billid, {
+    $scope.abill = [];
+    $http.jsonp('https://www.govtrack.us/api/v2/bill/' + billid + "?&format=jsonp", {
       params: {
         callback: 'JSON_CALLBACK'
       }
-    });
+    })
+      .success(function(data){
+        $scope.abill.push(data);
+        console.log(data);
+        console.log($scope.abill[0]);
+      })
+      .error(function(data, status, headers, config){
+        console.log("sorry data wasn't retrieved from govtrack bills");
+      });
 
   });
 
