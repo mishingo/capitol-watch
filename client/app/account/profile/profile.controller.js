bb7809d648cff3f376a77d97557626052aba2ee2
louisnovick <louisnovick@hotmail.com>
2015-04-26 23:52:56 -0400

    fixed login form and readded profile recent votes

--- client/app/account/profile/profile.controller.js
+++ client/app/account/profile/profile.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('capitolwatchApp')
   .controller('ProfileCtrl', function ($scope, User, Auth, $http) {
-
     $scope.errors = {};
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
@@ -11,38 +10,55 @@ angular.module('capitolwatchApp')
 
     $scope.bills = [];
     $http.jsonp('https://www.govtrack.us/api/v2/bill?order_by=-introduced_date&format=jsonp&limit=5', {
-        params: {
-            callback: 'JSON_CALLBACK'
-        }
+      params: {
+        callback: 'JSON_CALLBACK'
+      }
     })
-    .success(function (data) {
+      .success(function (data) {
         $http.get('/api/user_votes/ui/' + Auth.getCurrentUser()._id).success(function(user_votedata) {
-            console.log('/api/user_votes/ui/' + Auth.getCurrentUser()._id);
-            console.log(user_votedata);
-            for (var i = 0; i < data.objects.length; i++) {
-                var bill_id = data.objects[i].id;
+          console.log('/api/user_votes/ui/' + Auth.getCurrentUser()._id);
+          console.log(user_votedata);
+          for (var i = 0; i < data.objects.length; i++) {
+            var bill_id = data.objects[i].id;
             console.log(bill_id);
+          }
+        }).
+          error(function(data, status, headers, config) {
+            console.log("sorry post to api user_votes didnt work");
+          });
+
+        $http.post('/api/user_votes', {
+          userid: Auth.getCurrentUser()._id,
+          billid: '234',
+          stance: 'yea'
+        }).
+          success(function(data, status, headers, config) {
+            console.log("post to user_votes worked");
+            console.log(Auth.getCurrentUser()._id);
+          }).
+          error(function(data, status, headers, config) {
+            console.log("sorry post to api user_votes didnt work");
+          });
+
+       for (var i = 0; i < data.objects.length; i++) {
+          var bill = data.objects[i];
+          $scope.bills.push(bill);
+          console.log($scope.bills);
         }
-    })
-    .error(function(data, status, headers, config) {
-        console.log("sorry post to api user_votes didnt work");
-    });
+      });
 
 
-    $scope.uservotes = [];
-    $http.jsonp('/api/user_votes/ui/' + Auth.getCurrentUser()._id, {
-        params: {
-            callback: 'JSON_CALLBACK'
-        }
+  $scope.uservotes = [];
+    $http.get('/api/user_votes/ui/' + Auth.getCurrentUser()._id, {
+      params: {
+        callback: 'JSON_CALLBACK'
+      }
     })
     .success(function(data, status, headers, config) {
-        for (var i = 0; i < data.objects.length; i++) {
-            var uservote = data.objects[i];
-            $scope.uservotes.push(uservote);
-            console.log($scope.uservotes);
-        }
-    });
+      for (var i = 0; i < data.length; i++) {
+          var uservote = data[i];
+          $scope.uservotes.push(uservote);
+          console.log($scope.uservotes);
+      }
+    })
 });
-});
-
-
